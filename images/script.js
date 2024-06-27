18e9fccaf53aa6796d2cd82d405cf52340b3a41b
phynnoex <bankoletheophilus21@gmail.com>
2024-06-27 16:33:40 -0400

    Initial commit

---
+++ images/script.js
@@ -0,0 +1,16 @@
+//function to show menu when the menu icon is clicked
+let menuIcon = document.querySelector(".icon-border");
+let ismenu = true;
+
+menuIcon.addEventListener('click',function () {   
+    if (ismenu) {
+        document.querySelector(".nav-bar").style.display = 'flex';
+        ismenu = false
+        
+    }else{
+        document.querySelector(".nav-bar").style.display = 'none';
+        ismenu = true
+    }
+    console.log(ismenu)
+})
+
